fc04cfbef5f63c2852a1c56f31c08f49aec38725
IZsombor <ivadizs@kkszki.hu>
2023-09-14 10:49:40 +0200

    done

---
+++ ProductApi/Controllers/ProductsController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using static ProductApi.Dtos;
+
+namespace ProductApi.Controllers {
+    [Route("product")]
+    [ApiController]
+    public class ProductsController : ControllerBase {
+        private static readonly List<ProductDto> products = new() {
+            new ProductDto(Guid.NewGuid(), "Termék1", 2500, DateTimeOffset.UtcNow, DateTimeOffset.UtcNow),
+            new ProductDto(Guid.NewGuid(), "Termék2", 5500, DateTimeOffset.UtcNow, DateTimeOffset.UtcNow),
+            new ProductDto(Guid.NewGuid(), "Termék3", 12500, DateTimeOffset.UtcNow, DateTimeOffset.UtcNow)
+        };
+
+        [HttpGet]
+        public IEnumerable<ProductDto> GetAll() {
+            return products;
+        }
+
+        [HttpGet("{id}")]
+        public ProductDto GetById(Guid id) {
+            var product = products.Where(x => x.Id == id).FirstOrDefault();
+
+            return product;
+        }
+
+        [HttpPost]
+        public ProductDto PostProduct(CreateProductDto createProduct) {
+            var product = new ProductDto(
+                Guid.NewGuid(),
+                createProduct.ProductName,
+                createProduct.ProductPrice,
+                DateTimeOffset.UtcNow,
+                DateTimeOffset.UtcNow
+                );
+
+            products.Add(product);
+            return product;
+        }
+
+        [HttpPut]
+        public ProductDto PullProduct(Guid id, UpdateProductDto updateProduct) {
+            var existingProduct = products.Where(x => x.Id == id).FirstOrDefault();
+
+            var product = existingProduct with {
+                ProductName = updateProduct.ProductName,
+                ProductPrice = updateProduct.ProductPrice,
+                ModifiedTime = DateTimeOffset.UtcNow
+            };
+
+            var index = products.FindIndex(x => x.Id == id);
+
+            products[index] = product;
+            return product;
+        }
+
+        [HttpDelete]
+        public string  DeleteProduct(Guid id) {
+            var index = products.FindIndex(x => x.Id == id);
+            products.RemoveAt(index);
+
+            return "Termék törölve";
+        }
+    }
+}
---
+++ ProductApi/Dtos.cs
@@ -0,0 +1,10 @@
+﻿namespace ProductApi {
+    public class Dtos {
+        public record ProductDto(Guid Id, string ProductName, int ProductPrice, DateTimeOffset CreatedTime,
+            DateTimeOffset ModifiedTime);
+
+        public record CreateProductDto(string ProductName, int ProductPrice);
+
+        public record UpdateProductDto(string ProductName, int ProductPrice);
+    }
+}
